aa47e5c1989b61a669ffd95fd87c39dd21bc24bc
Federico Amorin <federicoamorin@gmail.com>
2021-03-15 22:10:30 -0300

    Add all recent changes

--- src/modeling.py
+++ src/modeling.py
@@ -57,14 +57,16 @@ def make_model(train_df_path: Path, output_path: Path) -> None:
         model_params_path (Path): Location of post hp-tunning parameters file.
         output_path (Path): Path to place final model in.
     """
+
     # Read train dataset and exctract features and target.
     train_df = pd.read_csv(train_df_path)
     x_train, y_train = train_df.drop("target", axis=1), train_df["target"]
     mask = (x_train.dtypes == "int64").tolist()
     x_train = x_train.loc[:, mask]
 
-    # Instanciate model with best hyperparams found.
-    model = LGBMClassifier()
+    # Instanciate model with best hyperparams found.\
+    params = PARAMS["model"]
+    model = LGBMClassifier(**params)
     x_train = x_train.rename(columns=lambda x: re.sub("[^A-Za-z0-9_]+", "", x))
     model = model.fit(x_train, y_train)
 
@@ -99,8 +101,7 @@ def predict(model_path: Path, test_df_path: Path, predict_path: Path) -> None:
 
 @app.command()
 def evaluate(
-    predict_file: Path,
-    test_df_path: Path,  # , scores_file: Path, plots_path: Path
+    predict_file: Path, test_df_path: Path, scores_file: Path, plots_path: Path
 ) -> None:
     """Compute and persist model metrics.
 
@@ -119,50 +120,49 @@ def evaluate(
     # Set threshhold and manipulate prediction array to leave it in the right
     # format.
     binary_preds = (predicts > params["thresh"]).astype(int)
+    pred_to_actual = pd.DataFrame(
+        {"predicted": binary_preds[:, 1].tolist(), "true": y_test.values.tolist()}
+    )
+
+    pred_to_actual.to_csv("data/output/results.csv", index=False)
 
     # Compute metrics and persist...
     confsn_matrix = confusion_matrix(y_test, binary_preds[:, 1])
-    print(confsn_matrix)
-    # roc_auc = roc_auc_score(y_test_dummies, binary_preds)
-    # num_cats = y_test_dummies.columns.shape[0]
-    # persist_scores(scores_file, confsn_matrix, roc_auc, num_cats)
+    roc_auc = roc_auc_score(y_test.values, binary_preds[:, 1])
+    persist_scores(scores_file, confsn_matrix, roc_auc, num_cats=2)
 
     # # Create and persist plots...
-    # persist_pr_curves(y_test_dummies, predicts, plots_path)
+    persist_pr_curves(y_test.values, predicts[:, 1], plots_path)
 
 
 def persist_scores(
     scores_file: Path, confsn_matrix: np.array, roc_auc: np.array, num_cats: int
 ):
+    print(type(roc_auc))
     """Persist model scores in json format"""
     with open(scores_file, "w") as fd:
         true_positives_dict = {
-            f"true_pos_{i}": str(confsn_matrix[i][i]) for i in range(num_cats)
+            f"true_pos_{i}": float(confsn_matrix[i][i]) for i in range(num_cats)
         }
         true_positives_dict["roc_auc"] = roc_auc
         json.dump(true_positives_dict, fd, indent=4)
 
 
-def persist_pr_curves(
-    y_test_dummies: pd.DataFrame, predicts: np.array, plots_path: Path
-):
+def persist_pr_curves(y_test: np.array, predicts: np.array, plots_path: Path):
     """Make and persist precision recall curve for each category in json."""
-    for category in y_test_dummies.columns:
-        file_name = f"pr_curve_{category}.json"
-        with open(plots_path / file_name, "w") as fd:
-            precision, recall, prc_thresholds = precision_recall_curve(
-                y_test_dummies.loc[:, category], predicts[:, category]
-            )
-            json.dump(
-                {
-                    "prc": [
-                        {"precision": p, "recall": r, "threshold": t}
-                        for p, r, t in zip(precision, recall, prc_thresholds)
-                    ]
-                },
-                fd,
-                indent=4,
-            )
+
+    with open(plots_path, "w") as fd:
+        precision, recall, prc_thresholds = precision_recall_curve(y_test, predicts)
+        json.dump(
+            {
+                "prc": [
+                    {"precision": p, "recall": r, "threshold": t}
+                    for p, r, t in zip(precision, recall, prc_thresholds)
+                ]
+            },
+            fd,
+            indent=4,
+        )
 
 
 if __name__ == "__main__":
